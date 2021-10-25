3cd11fe6fe03a36198634fd3a203fe8a8bf23586
oscar <moscurru@gmail.com>
2021-10-25 09:37:06 +0200

    d

---
+++ docu.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Pretend this is a file
+ *
+ * Page-level DocBlock is here because it is the first DocBlock
+ * in the file, and is immediately followed by the second
+ * DocBlock before any documentable element is declared
+ * (function, define, class, global variable, include)
+ * @package pagepackage
+ */
+/**
+ * Here is the class DocBlock
+ *
+ * The class package is different from the page package!
+ * @package classpackage
+ */
+class myclass
+{
+}
+?>
